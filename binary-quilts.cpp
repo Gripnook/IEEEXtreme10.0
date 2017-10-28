70194ef5b837bb122e16da05d9045dc1d3eb9ab2
Gripnook <XxAndreixX@hotmail.com>
2017-10-28 19:07:10 -0400

    Changed case of type

--- binary-quilts.cpp
+++ binary-quilts.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-using state = int;
+using State = int;
 using Set = vector<bool>;
-using Queue = vector<state>;
+using Queue = vector<State>;
 
-bool get(const state &board, int x, int y)
+bool get(const State &board, int x, int y)
 {
     return (board & (1 << ((5 * x) + y))) > 0;
 }
 
-void setbit(state &board, int x, int y, bool value)
+void setbit(State &board, int x, int y, bool value)
 {
     if (value)
     {
@@ -24,15 +24,15 @@ void setbit(state &board, int x, int y, bool value)
     }
 }
 
-void flip(state &board, int x, int y)
+void flip(State &board, int x, int y)
 {
     board ^= (1 << ((5 * x) + y));
 }
 
-void swap(state &board, int x1, int y1, int x2, int y2)
+void swap(State &board, int x1, int y1, int x2, int y2)
 {
-    state idx1 = (1 << ((5 * x1) + y1));
-    state idx2 = (1 << ((5 * x2) + y2));
+    State idx1 = (1 << ((5 * x1) + y1));
+    State idx2 = (1 << ((5 * x2) + y2));
     bool v1 = board & idx1;
     bool v2 = board & idx2;
     if (v1)
@@ -55,34 +55,34 @@ void swap(state &board, int x1, int y1, int x2, int y2)
 
 constexpr int masksH[]{0x01, 0x03, 0x07, 0x0F, 0x1F};
 
-void swapH(state &board, int x1, int x2, int y, int n)
+void swapH(State &board, int x1, int x2, int y, int n)
 {
     int xx1 = (x1 << 2) + x1;
     int xx2 = (x2 << 2) + x2;
     int delta = xx2 - xx1;
-    state idx1 = masksH[n - 1] << (xx1 + y);
-    state idx2 = masksH[n - 1] << (xx2 + y);
-    state v1 = (board & idx1) << (delta);
-    state v2 = (board & idx2) >> (delta);
+    State idx1 = masksH[n - 1] << (xx1 + y);
+    State idx2 = masksH[n - 1] << (xx2 + y);
+    State v1 = (board & idx1) << (delta);
+    State v2 = (board & idx2) >> (delta);
     board &= ~(idx1 | idx2);
     board |= v1 | v2;
 }
 
 constexpr int masksV[]{0x00000001, 0x00000021, 0x00000421, 0x00008421, 0x00108421};
 
-void swapV(state &board, int x, int y1, int y2, int n)
+void swapV(State &board, int x, int y1, int y2, int n)
 {
     int xx = (x << 2) + x;
     int delta = (y2 - y1);
-    state idx1 = masksV[n - 1] << (xx + y1);
-    state idx2 = masksV[n - 1] << (xx + y2);
-    state v1 = (board & idx1) << (delta);
-    state v2 = (board & idx2) >> (delta);
+    State idx1 = masksV[n - 1] << (xx + y1);
+    State idx2 = masksV[n - 1] << (xx + y2);
+    State v1 = (board & idx1) << (delta);
+    State v2 = (board & idx2) >> (delta);
     board &= ~(idx1 | idx2);
     board |= v1 | v2;
 }
 
-state flipH(state board, int x0, int y0, int n)
+State flipH(State board, int x0, int y0, int n)
 {
     int cl = (x0 << 1) + n - 1;
     for (int i = x0; i < x0 + n / 2; ++i)
@@ -92,7 +92,7 @@ state flipH(state board, int x0, int y0, int n)
     return board;
 }
 
-state flipV(state board, int x0, int y0, int n)
+State flipV(State board, int x0, int y0, int n)
 {
     int cl = (y0 << 1) + n - 1;
     for (int j = y0; j < y0 + n / 2; ++j)
@@ -104,13 +104,13 @@ state flipV(state board, int x0, int y0, int n)
 
 constexpr int masksInv[]{0x00000001, 0x00000063, 0x00001CE7, 0x0007BDEF, 0x01FFFFFF};
 
-state invert(state board, int x0, int y0, int n)
+State invert(State board, int x0, int y0, int n)
 {
-    state idx = masksInv[n - 1] << ((5 * x0) + y0);
+    State idx = masksInv[n - 1] << ((5 * x0) + y0);
     return board ^ idx;
 }
 
-bool add(Queue &q, const state &board, Set &dp, Set &otherdp)
+bool add(Queue &q, const State &board, Set &dp, Set &otherdp)
 {
     for (int n = 1; n <= 5; ++n)
     {
@@ -159,7 +159,7 @@ Queue back;
 vector<bool> dpfront(1 << 25);
 vector<bool> dpback(1 << 25);
 
-int search(const state &initial, const state &goal)
+int search(const State &initial, const State &goal)
 {
     if (initial == goal)
     {
@@ -213,8 +213,8 @@ int main()
     back.reserve(1 << 25);
     while (t--)
     {
-        state initial{};
-        state goal{};
+        State initial{};
+        State goal{};
         char ch;
         for (int i = 0; i < 5; ++i)
         {
